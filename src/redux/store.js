314709ca163c424278a533c5439dfb7bedf7b153
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-07-18 17:42:51 +0300

    fixed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,30 +1,15 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import userContacts from './contact-reduce';
-import storage from 'redux-persist/lib/storage';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
+
 
 const middleware = [
-  ...getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
+  ...getDefaultMiddleware(),
   logger,
 ];
 
 const contactsPersistor = {
   key: 'contacts',
-  storage,
   blacklist: ['filter'],
 };
 
