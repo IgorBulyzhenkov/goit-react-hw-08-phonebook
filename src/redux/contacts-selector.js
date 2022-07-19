61ce6b6692aec7e33e37180314bc86eb43954369
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-07-19 15:47:29 +0300

    final work

--- src/redux/contacts-selector.js
+++ src/redux/contacts-selector.js
@@ -1,26 +1,48 @@
-// const allContacts = state => state.contacts.iytems;
+import { createSelector } from '@reduxjs/toolkit';
+
 export const getContacts = state => state.contacts.items;
 export const getFilter = state => state.contacts.filter;
 export const getError = state => state.contacts.error;
 export const getLoading = state => state.contacts.loading;
 
-const sortContacts = state => {
-  const contacts = getContacts(state);
+// const sortContacts = state => {
+//   const contacts = getContacts(state);
+//   const sorted = [...contacts]?.sort((a, b) => {
+//     const aDate = Date.parse(a.createdAt);
+//     const bDate = Date.parse(b.createdAt);
+//     return bDate - aDate;
+//   });
+
+//   return sorted;
+// };
+
+// export const getVisibleFilterContacts = state => {
+//   const contacts = sortContacts(state);
+//   const filter = getFilter(state);
+// const normalizeFilter = filter?.toLowerCase();
+
+// return contacts.filter(contact =>
+//   contact?.name?.toLowerCase().includes(normalizeFilter)
+// );
+// };
+
+const sortContacts = createSelector(getContacts, contacts => {
   const sorted = [...contacts]?.sort((a, b) => {
     const aDate = Date.parse(a.createdAt);
     const bDate = Date.parse(b.createdAt);
     return bDate - aDate;
   });
-
   return sorted;
-};
+});
 
-export const getVisibleFilterContacts = state => {
-  const contacts = sortContacts(state);
-  const filter = getFilter(state);
-  const normalizeFilter = filter?.toLowerCase();
+export const getVisibleFilterContacts = createSelector(
+  sortContacts,
+  getFilter,
+  (contacts, filter) => {
+    const normalizeFilter = filter?.toLowerCase();
 
-  return contacts.filter(contact =>
-    contact?.name?.toLowerCase().includes(normalizeFilter)
-  );
-};
+    return contacts.filter(contact =>
+      contact?.name?.toLowerCase().includes(normalizeFilter)
+    );
+  }
+);
