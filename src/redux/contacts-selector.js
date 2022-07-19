ff10469391d4fbdced652c49d9bf2203af5174c6
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-07-19 17:29:16 +0300

    selektors

--- src/redux/contacts-selector.js
+++ src/redux/contacts-selector.js
@@ -5,27 +5,6 @@ export const getFilter = state => state.contacts.filter;
 export const getError = state => state.contacts.error;
 export const getLoading = state => state.contacts.loading;
 
-// const sortContacts = state => {
-//   const contacts = getContacts(state);
-//   const sorted = [...contacts]?.sort((a, b) => {
-//     const aDate = Date.parse(a.createdAt);
-//     const bDate = Date.parse(b.createdAt);
-//     return bDate - aDate;
-//   });
-
-//   return sorted;
-// };
-
-// export const getVisibleFilterContacts = state => {
-//   const contacts = sortContacts(state);
-//   const filter = getFilter(state);
-// const normalizeFilter = filter?.toLowerCase();
-
-// return contacts.filter(contact =>
-//   contact?.name?.toLowerCase().includes(normalizeFilter)
-// );
-// };
-
 const sortContacts = createSelector(getContacts, contacts => {
   const sorted = [...contacts]?.sort((a, b) => {
     const aDate = Date.parse(a.createdAt);
