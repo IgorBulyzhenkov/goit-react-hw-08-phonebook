41b8b306532a3558215d38886d4e74de8a0da400
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-07-23 16:23:29 +0300

    fixed 2.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,30 @@
+import { lazy, Suspense, useEffect } from 'react';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import { Routes, Route } from 'react-router-dom';
-import { useEffect } from 'react';
+import s from './App.module.css';
+import { Oval } from 'react-loader-spinner';
 import { useDispatch, useSelector } from 'react-redux';
-import Header from './Header/Header';
-import Contacts from './Contacts/Contacts';
-import Register from './Register/Register';
-import Login from './Login/Login';
 import userFetch from '../redux/user/user-operation';
 import PrivetRoute from './Route/PrivateRoute';
 import PublicRoute from './Route/PublicRoute';
 import { getIsFetchingCurrent, getName } from 'redux/user/user-selector';
+// import Header from './Header/Header';
+// import Contacts from './Contacts/Contacts';
+// import Register from './Register/Register';
+// import Login from './Login/Login';
+
+const Header = lazy(() => {
+  import('./Header/Header' /* webpackChunkName: "Header" */);
+});
+const Contacts = lazy(() => {
+  import('./Contacts/Contacts' /* webpackChunkName: "Contacts" */);
+});
+const Register = lazy(() => {
+  import('./Register/Register' /* webpackChunkName: "Register" */);
+});
+const Login = lazy(() => {
+  import('./Login/Login' /* webpackChunkName: "Login" */);
+});
 
 const { fetchCurrentUser } = userFetch;
 
@@ -22,10 +37,14 @@ function App() {
     dispatch(fetchCurrentUser());
   }, [dispatch]);
 
-  return (
-    !isFetching && (
-      <>
-        <Header />
+  return isFetching ? (
+    <div className={s.Loader}>
+      <Oval color="#1b181b" height={70} width={70} secondaryColor="grey" />
+    </div>
+  ) : (
+    <>
+      <Header />
+      <Suspense>
         <Routes>
           <Route
             path="register"
@@ -55,8 +74,8 @@ function App() {
             <Route path="*" element={userName ? <Contacts /> : <Register />} />
           )}
         </Routes>
-      </>
-    )
+      </Suspense>
+    </>
   );
 }
 
